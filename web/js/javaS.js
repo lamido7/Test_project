fffc7d97732938cd0fa85d9a8bfad6afb31597ad
Lamido7 <konim58@gmail.com>
2021-11-02 12:30:22 +0100

    JS Assignment

--- web/js/javaS.js
+++ web/js/javaS.js
@@ -16,3 +16,15 @@ function Type(x){
    return typeof(x)
 }
 console.log(Type('true'))
+
+function areaTri(){
+    var side1 = 5
+    var side2 = 6
+    var side3 = 7
+
+    var s = (side1 + side2 + side3) / 2
+
+    var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)))
+    return area
+}console.log(areaTri())
+
