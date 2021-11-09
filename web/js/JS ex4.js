fc71e0336c8bda0104ea18d5b2bed4ca2a282193
Lamido7 <konim58@gmail.com>
2021-11-09 10:47:02 +0100

    JS assignment

--- web/js/JS ex4.js
+++ web/js/JS ex4.js
@@ -10,3 +10,16 @@ function code(str){
     }
 }
 console.log(code('a0b'))
+
+function oddEven(){
+
+    for ( var i = 0; i < 15; i++){
+        if(i % 2 == 0){
+            alert('Even number ' + i)
+        }else{
+            alert('Odd number ' + i)
+        }
+    }
+
+}
+oddEven()
